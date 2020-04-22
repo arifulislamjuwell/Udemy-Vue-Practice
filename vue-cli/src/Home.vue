a55f4f2e598f6592aa53e5f6a0c529ab25c5e056
juwel <juwelariful1@gmail.com>
2020-04-22 23:50:02 +0600

    practicing component on cli

---
+++ vue-cli/src/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>  
+        <p>server status: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">change status</button>
+    </div>
+</template>
+<script>
+export default {
+    data: function (){
+        return{
+            status: 'critical'
+        }
+    },
+    methods:{
+        changeStatus(){
+            this.status= 'normal'
+        }
+    }
+}
+</script>
