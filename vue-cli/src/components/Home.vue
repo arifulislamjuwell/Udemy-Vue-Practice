43f10255f4005ca84bf939536ffadf84622ca369
juwel <juwelariful1@gmail.com>
2020-04-23 22:44:30 +0600

    vue component cli structure and global, local practice

---
+++ vue-cli/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>  
+        <check-server-status v-for="server in 5" :key="server"> </check-server-status>
+    </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus.vue';
+export default {
+    components:{
+        'check-server-status': ServerStatus                                                                                                                                                                                                                                                                          
+    }
+}
+</script>
