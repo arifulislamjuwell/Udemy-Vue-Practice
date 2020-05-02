45384794173398c059e68a2bc7b923d737b06dcd
juwel <juwelariful1@gmail.com>
2020-05-02 23:27:24 +0600

    user view setup for comminucate two components

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,20 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>
         <div class="row">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
+            <div class="col-xs-12">
+                <app-user></app-user>
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
+
 <script>
-    import Header from './components/Shared/Header.vue'
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue'
+    import UserApp from './components/user/User.vue'
 
     export default {
         components: {
-            appHeader: Header,
-            appServers: Servers,
-            appServerDetails: ServerDetails,
-            appFooter: Footer
+            appUser: UserApp,
         }
     }
 </script>
