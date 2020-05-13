c95d8b48b1994e18005b5541e3691f196856939f
juwel <juwelariful1@gmail.com>
2020-05-13 23:05:29 +0600

    build form for vue control

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Form from './components/FormControl.vue'
+
 
 export const eventBus = new Vue();
 
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(Form)
 })
