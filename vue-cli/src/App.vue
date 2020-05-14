d8c3e2b632aee1a6fee774344b7ca8397fb4282d
juwel <juwelariful1@gmail.com>
2020-05-15 03:02:29 +0600

    create custom directive globally and local

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,50 +1,45 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <button @click="selectedComponent= 'appQuote'"> default</button>
-                <button @click="selectedComponent= 'AuthorQuote'"> Author </button>
-                <button @click="selectedComponent= 'NewApp'"> New </button>
-                {{ selectedComponent }}
-                <keep-alive>
-                        <component :is="selectedComponent"></component>
-                </keep-alive >
-
-                <!-- <app-quote>
-                    <h1 slot="title">A wonderful quote</h1>
-                    <span>
-                        <slot> r yerh </slot>
-                    </span>
-                    <div>
-                    <p> content </p>
-                    <p> content </p>
-                    </div>
-                </app-quote> -->
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built in Directive</h1>
+        <p v-text="'some text'"></p>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom in Directive</h1>
+        <p v-highlight:background.delayed="'yellow'">color this</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
-    import Author from './components/quote/Author'
-    import Newapp from './components/quote/New'
-    import Quote from './components/quote/Quote'
-
-    export default {
-        data: function(){
-            return {
-                selectedComponent: 'appQuote'
-            }
-        },
-        components: {
-            appQuote: Quote,
-            AuthorQuote: Author,
-            NewApp: Newapp
+export default {
+  directives: {
+      'highlight':{
+              bind(el, binding, vnode) {
+        //  el.style.backgroundColor= 'gray'
+        var delay = 0
+        if (binding.modifiers['delayed']) {
+            delay = 3000
         }
+        setTimeout(() => {
+            if (binding.arg) {
+                el.style.backgroundColor = binding.value
+            }
+            else {
+                el.style.color = binding.value
+            }
+
+        }, delay)
     }
+      }
+  }
+};
 </script>
 
 <style>
-
 </style>
---
+++ vue-cli/src/components/quote/QuoteDashboard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent= 'appQuote'"> default</button>
+                <button @click="selectedComponent= 'AuthorQuote'"> Author </button>
+                <button @click="selectedComponent= 'NewApp'"> New </button>
+                {{ selectedComponent }}
+                <keep-alive>
+                        <component :is="selectedComponent"></component>
+                </keep-alive >
+
+                <!-- <app-quote>
+                    <h1 slot="title">A wonderful quote</h1>
+                    <span>
+                        <slot> r yerh </slot>
+                    </span>
+                    <div>
+                    <p> content </p>
+                    <p> content </p>
+                    </div>
+                </app-quote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import Author from './Author'
+    import Newapp from './New'
+    import Quote from './Quote'
+    export default {
+        data: function(){
+            return {
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            AuthorQuote: Author,
+            NewApp: Newapp
+        }
+    }
+</script>
+
+<style>
+
+</style>
