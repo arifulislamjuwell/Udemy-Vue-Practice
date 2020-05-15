502ebf05a7998f43412556020a3a7afe690fc9eb
juwel <juwelariful1@gmail.com>
2020-05-15 21:21:34 +0600

    multiple modifires directive and add gitignore file

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,44 +1,17 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Built in Directive</h1>
-        <p v-text="'some text'"></p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Custom in Directive</h1>
-        <p v-highlight:background.delayed="'yellow'">color this</p>
-      </div>
-    </div>
-  </div>
+<div>
+<custom-directive></custom-directive>
+</div>
 </template>
 
 
 <script>
+import Directive from './components/CustomDirective'
 export default {
-  directives: {
-      'highlight':{
-              bind(el, binding, vnode) {
-        //  el.style.backgroundColor= 'gray'
-        var delay = 0
-        if (binding.modifiers['delayed']) {
-            delay = 3000
-        }
-        setTimeout(() => {
-            if (binding.arg) {
-                el.style.backgroundColor = binding.value
-            }
-            else {
-                el.style.color = binding.value
-            }
-
-        }, delay)
-    }
-      }
+  components:{
+    customDirective: Directive
   }
-};
+}
 </script>
 
 <style>
---
+++ vue-cli/src/components/CustomDirective.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built in Directive</h1>
+        <p v-text="'some text'"></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom in Directive</h1>
+        <p v-highlight:background.delayed.blink="{mainColor: 'yellow', secondColor: 'gray', delay : '500'}">color this</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  directives: {
+    highlight: {
+      bind(el, binding, vnode) {
+        //  el.style.backgroundColor= 'gray'
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = binding.value.delay
+        }
+        console.log(binding.value)
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == mainColor ? currentColor = secondColor : currentColor = mainColor
+              if (binding.arg) {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, binding.value.delay);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg) {
+              el.style.backgroundColor = binding.value.mainColor;
+            } else {
+              el.style.color = binding.value.mainColor;
+            }
+          }, delay);
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
